1051999109cd4801cb1aac44182460fc7c7c84f4
lucastito <lucas.s.tito@gmail.com>
2014-12-06 03:03:36 -0200

    os métodos de envio foram adaptados para enviar data também

--- Java/Anjo_da_Guarda/src/metodo_de_envio/Metodo.java
+++ Java/Anjo_da_Guarda/src/metodo_de_envio/Metodo.java
@@ -1,14 +1,20 @@
 package metodo_de_envio;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public abstract class Metodo {
 	protected Map<String, String> mapCausaConsequencia = new HashMap<String, String>();
 	protected List<Integer> imeis;
-protected List<Integer> celulares;
+	protected List<Integer> celulares;
+	protected DateTimeFormatter formatador = DateTimeFormatter
+			.ofLocalizedDateTime(FormatStyle.MEDIUM).withLocale(
+					new Locale("pt", "br"));
 
-public abstract void actionByString(String inputLine);
+	public abstract void actionByString(String inputLine);
 
 }
